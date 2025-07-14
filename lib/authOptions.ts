bfce2545b35809bc45e2643627ece8d8955f2f11
catalano00 <j.catalano114@gmail.com>
2025-07-14 16:09:50 -0400

    'update authOptions'

---
+++ lib/authOptions.ts
@@ -0,0 +1,20 @@
+// src/lib/authOptions.ts
+import { type NextAuthOptions } from 'next-auth';
+import AzureADProvider from 'next-auth/providers/azure-ad';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID!,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+      tenantId: process.env.AZURE_AD_TENANT_ID!,
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
